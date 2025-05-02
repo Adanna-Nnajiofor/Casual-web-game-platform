c0b35753239e52c39457a75dd40efd5eb29ca09a
Adanna-Nnajiofor <adanna.nnajiofor@gmail.com>
2025-05-02 12:52:11 +0100

    fix: corrected issue with [firebase-admin.ts]

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -1,21 +1,14 @@
 import * as admin from "firebase-admin";
-import * as fs from "fs";
-import * as path from "path";
 
-// Load environment variables
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
 
-if (!serviceAccountPath) {
-  throw new Error("FIREBASE_SERVICE_ACCOUNT_PATH is not defined in .env");
+if (!serviceAccountString) {
+  throw new Error(
+    "FIREBASE_SERVICE_ACCOUNT_JSON is not defined in environment variables"
+  );
 }
 
-const fullPath = path.resolve(serviceAccountPath);
-
-if (!fs.existsSync(fullPath)) {
-  throw new Error(`Service account file not found at path: ${fullPath}`);
-}
-
-const serviceAccount = JSON.parse(fs.readFileSync(fullPath, "utf8"));
+const serviceAccount = JSON.parse(serviceAccountString);
 
 if (!admin.apps.length) {
   admin.initializeApp({
